84720dd2b3002363e48732384808bd5895b28db9
Dave Goddard <dave@goddard.id.au>
2013-09-04 00:29:16 -0400

    enable history api

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,3 +1,7 @@
+App.Router.reopen({
+    location: 'history'
+});
+
 App.Router.map(function() {
   this.resource('products');
   this.resource('product', {path: 'products/:product_id'});
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 YoungLiving::Application.routes.draw do
   resources :products, only: [ :index, :show ]
   resources :categories, only: [ :index, :show ]
+
   root :to => 'application#index'
 end
